8f48e784793eb10009a0ad8efab8a96087106cce
TinyikoNdlovu <tinyiko13@yahoo.com>
2023-03-27 15:44:16 +0200

    first commit

---
+++ dict.py
@@ -0,0 +1,8 @@
+
+
+personalDetails = {"FirstName": "James", "LastName": "Ouko", "Age": 22, "PhoneNumber": "07123456789", "Email": "student@gmail.com"} 
+
+personalDetails["Age"] = 55
+print("Updated Age is: ",personalDetails)
+print("Last Name is: ",personalDetails["LastName"])
+print("Length of the Dictionart is: ",len(personalDetails))
